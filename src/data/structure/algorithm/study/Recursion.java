a6e4a4992ac8a87db6448313637656c33cffc178
ijuhom <2014brand@naver.com>
2019-02-05 23:11:49 +0900

    Recursion

---
+++ src/data/structure/algorithm/study/Recursion.java
@@ -0,0 +1,11 @@
+package data.structure.algorithm.study;
+
+public class Recursion {
+	public static void main(String[] args) {
+		func();
+	}
+	public static void func() {
+		System.out.println("hello");
+		func();
+	}
+}
