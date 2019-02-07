a01fa34d9cf064577c9a0352d672223444d6c45b
ijuhom <2014brand@naver.com>
2019-02-08 01:02:48 +0900

    Recursion Eucild Method

---
+++ src/data/structure/algorithm/study/Recursion7.java
@@ -0,0 +1,34 @@
+package data.structure.algorithm.study;
+
+/**
+ * func(12,40) = func(40 ,12)
+ * func(40,12) = func(12, 6)
+ * func(12, 6) = func( 6, 0)
+ * func( 6, 0) = 6
+ * Euclid Method
+ */
+public class Recursion7 {
+	public static void main(String[] args) {
+		int x = 12;
+		int y = 40;
+		int result = func(x, y);
+		System.out.println(result);
+	}
+
+//	public static int func(int x, int y) {
+//		if (x<y) {
+//			int temp =y; y=x; x=temp;
+//		}	
+//		if (x%y==0) 
+//			return y;
+//		else
+//			return func(y ,x%y);		
+
+	public static int func(int x, int y) {
+		if(y==0)
+			return x;
+		else
+			return func(y ,x%y);
+		
+	}
+}
