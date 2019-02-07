17113ba1161e2df00adea34be2385ed4996290d1
ijuhom <2014brand@naver.com>
2019-02-07 23:40:25 +0900

    recursion sum n~1

--- src/data/structure/algorithm/study/Recursion2.java
+++ src/data/structure/algorithm/study/Recursion2.java
@@ -1,5 +1,8 @@
 package data.structure.algorithm.study;
-
+/* 무한루프에 빠지지않으려면 2조건을 충족해야한다.
+ *  Base case : (if 해당)적어도 하나의 recursion에 빠지지 않는 경우가 존재해야한다.
+ * Recursive case  :(else 해당) recursion을 반복하다보면 결국 basecase로 수렴해야한다.
+ * */
 public class Recursion2 {
 	public static void main(String[] args) {
 		func(2);									//hello 2번출력
---
+++ src/data/structure/algorithm/study/Recursion3.java
@@ -0,0 +1,23 @@
+package data.structure.algorithm.study;
+
+/**
+ * func(4) 반환값은 4 + func(3) 
+ * func(3) 반환값 3 + func(2)
+ * ...
+ * func(0) 반환값 0
+ * 결국 4 + 3 + 2 + 1 이된다. 1-n 까지의 합을 구한다.
+ */
+public class Recursion3 {
+	public static void main(String[] args) {
+		int result = func(3);
+		System.out.println(result);
+	}
+
+	public static int func(int n) {
+		if (n == 0) {
+			return 0;
+		} else {
+			return n + func(n - 1);
+		}
+	}
+}
