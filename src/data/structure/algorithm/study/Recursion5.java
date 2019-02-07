966b73a7df8ee91d94f305c3a7efef8776b15ce9
ijuhom <2014brand@naver.com>
2019-02-08 00:09:47 +0900

    Recursion x*x^(n-1)

---
+++ src/data/structure/algorithm/study/Recursion5.java
@@ -0,0 +1,25 @@
+package data.structure.algorithm.study;
+
+/**
+ * func(3 , 3) return 3 * func(3 , 2) 
+ * func(3 , 2) return 3 * func(3 , 1)
+ * func(3 , 1) return 3 * func(3 , 0)
+ * func(3 , 0) return 1
+ * result x*x^(n-1)
+ */
+public class Recursion5 {
+	public static void main(String[] args) {
+		int n = 3; 
+		int x =3;		
+		int result = func(x,n);
+		System.out.println(result);
+	}
+
+	public static int func(int x,int n) {
+		if (n == 0) {
+			return 1;
+		} else {
+			return x * func(x,n-1);
+		}
+	}
+}
