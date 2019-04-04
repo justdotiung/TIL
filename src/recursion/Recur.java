eddcc1f3dd455b3dbe762161d2b5c1ca2a741234
ijuhom <2014brand@naver.com>
2019-04-04 23:22:03 +0900

    꼬리 재귀와 비재귀적 표현 으로 바꾸는 구조의 이해

--- src/recursion/Recur.java
+++ src/recursion/Recur.java
@@ -2,6 +2,8 @@ package recursion;
 
 import java.util.Scanner;
 
+import recursion.IntStack.EmptyIntStackException;
+
 public class Recur {
 	static void recur(int n) {
 		if (n > 0) {
@@ -10,12 +12,41 @@ public class Recur {
 			recur(n - 2);
 		}
 	}
-	
-	public static void main(String[] args) {
+	static void recur2(int n) {
+		while(n>0) {
+			recur (n-1);
+			System.out.println(n);
+			n = n-2;
+		}
+	}
+	static void recur3(int n) throws EmptyIntStackException {
+		IntStack s = new IntStack(n);
+		
+		while(true) {
+			if(n>0) {
+				s.push(n);
+				n=n-1;
+				continue;
+			}
+			if(s.isEmpty() != true) {
+				n =s.pop();
+				System.out.println(n);
+				n=n-2;
+				continue;
+			}
+			break;
+		}
+	}
+	public static void main(String[] args) throws EmptyIntStackException {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("정수를 입력하세요: ");
 		int x = sc.nextInt();
+		recur2(x);
+		System.out.println();
 		recur(x);
+		System.out.println();
+		recur3(x);
 	}
+	
 }
