b9a2997686375f75bc2259b7988e724347170ba5
ijuhom <2014brand@naver.com>
2019-04-07 19:31:40 +0900

    슈퍼타입 토큰 개선 및 이해 2.

--- java/coding/SuperTypeTokenEx.java
+++ java/coding/SuperTypeTokenEx.java
@@ -14,17 +14,17 @@ import coding.TypeTokenEx.TypeSafeMap;
 
 public class SuperTypeTokenEx {
 	static class TypeSafeMap {
-		Map<TypeReference<?>, Object> map = new HashMap<>();
+		Map<Type, Object> map = new HashMap<>();
 
 		<T> void put(TypeReference<T> tr, T value) {
-			map.put(tr, value);
+			map.put(tr.type, value);
 		}
 
 		<T> T get(TypeReference<T> tr) {
 			if (tr.type instanceof Class<?>)
-				return ((Class<T>) tr.type).cast(map.get(tr));// tr<String>
+				return ((Class<T>) tr.type).cast(map.get(tr.type));// tr<String>
 			else
-				return ((Class<T>) ((ParameterizedType) tr.type).getRawType()).cast(map.get(tr)); // tr<List<String>>
+				return ((Class<T>)((ParameterizedType) tr.type).getRawType()).cast(map.get(tr.type)); //type 은 지네릭 정보를 보두 가지고 있다.
 		}
 	}
 
@@ -72,15 +72,19 @@ public class SuperTypeTokenEx {
 		};
 		System.out.println(t.type);
 		TypeSafeMap map = new TypeSafeMap();
-		map.put(new TypeReference<String>() {
-		}, "123");
-		map.put(new TypeReference<Integer>() {
-		}, 1);
-		map.put(new TypeReference<List<String>>() {
-		}, Arrays.asList("1", "2", "3")); // List<String>
-		map.put(new TypeReference<List<Integer>>() {
-		}, Arrays.asList(1, 2, 3));// List<Integer>
+		map.put(new TypeReference<String>() {}, "123");
+		map.put(new TypeReference<Integer>() {}, 1);
+		map.put(new TypeReference<List<String>>() {}, Arrays.asList("1", "2", "3")); // List<String>
+		map.put(new TypeReference<List<Integer>>() {}, Arrays.asList(1, 2, 3));// List<Integer>
+		map.put(new TypeReference<List<List<String>>>() {}, Arrays.asList(
+				Arrays.asList("a","b"),Arrays.asList("c","d"),Arrays.asList("e")));// 3중으로 지네릭 타입을 가지고 있어도 알아낼수있다.
 
+		Map<String,String> m = new HashMap<>();
+		m.put("key1","value1");
+		m.put("key2","value2");
+		map.put(new TypeReference<Map<String,String>>(){}, m);
+		
+		
 		System.out.println(map.get(new TypeReference<String>() {
 		}));
 		System.out.println(map.get(new TypeReference<Integer>() {
@@ -89,6 +93,11 @@ public class SuperTypeTokenEx {
 		}));
 		System.out.println(map.get(new TypeReference<List<Integer>>() {
 		}));
-		// 이러한 단점을 보완하기위해 SuperTypeToke 을 알아보자.
+		System.out.println(map.get(new TypeReference<List<List<String>>>() {
+		}));
+		System.out.println(map.get(new TypeReference<Map<String,String>>(){}));
+		
+		
+	
 	}
 }
