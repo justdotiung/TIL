b94d5cdcf8f7a9509e74691bf552884480fb6c07
ijuhom <2014brand@naver.com>
2019-04-08 23:31:18 +0900

    Generic 개념및 이해 1

--- java/generic/java/Generics.java
+++ java/generic/java/Generics.java
@@ -15,13 +15,13 @@ public class Generics {
 	}
 
 	public static void main(String[] args) {
-	//	System.out.println(new Hello<String>().print("하이")); //type argument
+		System.out.println(new Hello<String>().print("하이")); //type argument
 		//Raw Type
-	//	List list = new ArrayList<Integer>(); //선언부 생략 동작가능 
+		List list = new ArrayList<Integer>(); //선언부 생략 동작가능 
 		List<Integer> ints = Arrays.asList(1,2,3);
-		List rawInts =ints;
+		List rawInts =ints;	//요소를 알수없기때문에 Raw Type 은 위험 할 수있다.
 		List<Integer> ints2 = rawInts;
-		List<String> ints3 = rawInts;
-		String str =  ints3.get(1);
+		List<String> ints3 = rawInts;	//element를 알수없다 컴파일시 에러는 안나지만 실사용시 에러 나옴
+		String str =  ints3.get(1);		// 에러.
 	}
 }
