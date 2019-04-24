b6f769ff6c74630c827b23972b9c9c3b5f7bca63
ijuhom <2014brand@naver.com>
2019-04-24 23:51:00 +0900

    InsertionSort 구조

---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,37 @@
+package sorting;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+	static void insertionSort(int[] a, int n) {
+		for (int i = 1; i < n; i++) {
+			int j;
+			int tmp = a[i];
+			for (j = i; j > 0 && a[j - 1] > tmp; j--) {
+				a[j] = a[j - 1];
+			}
+			a[j] = tmp;
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("단순 삽입 정렬");
+		System.out.print("요소수: ");
+		int nx = sc.nextInt();
+		int[] x = new int[nx];
+
+		for (int i = 0; i < nx; i++) {
+			System.out.print("x[" + i + "]:");
+			x[i] = sc.nextInt();
+		}
+
+		insertionSort(x, nx);
+
+		System.out.println("오름차순으로 정렬");
+		for (int i = 0; i < nx; i++) {
+			System.out.println("x[" + i + "]=" + x[i]);
+		}
+	}
+}
