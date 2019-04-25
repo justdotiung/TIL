0e8022b0a7fede950da00d65aade995815c61e01
ijuhom <2014brand@naver.com>
2019-04-25 23:01:29 +0900

    shellSort 기본구조

---
+++ src/sorting/ShellSort.java
@@ -0,0 +1,35 @@
+package sorting;
+
+import java.util.Scanner;
+
+public class ShellSort {
+	static void shellSort(int[] a, int n) {
+		for(int h = n/2; h > 0 ; h/=2)
+			for(int i =h; i<n; i++) {
+				int j;
+				int tmp = a[i];
+				for(j=i-h; j>=0 && a[j] > tmp; j-=h)
+					a[j+h] =a[j];
+				a[j+h] = tmp;
+			}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("셀정렬(버전1)");
+		System.out.print("요소수: ");
+		int nx = sc.nextInt();
+		int[] x = new int[nx];
+		
+		for(int i = 0; i < nx; i++) {
+			System.out.print("x["+i+"]: ");
+			x[i] = sc.nextInt();
+		}
+		
+		shellSort(x, nx);
+		
+		System.out.println("오름차순으로 정렬했습니다.");
+		for(int i = 0; i<nx;i++)
+			System.out.println("x["+i+"]="+x[i]);
+	}
+}
