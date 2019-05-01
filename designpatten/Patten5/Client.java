284b90ea664765f3b220cd1d932a03b6df970e32
ijuhom <2014brand@naver.com>
2019-05-01 21:43:42 +0900

    옵서버 패턴 3 동시에 출력하는 경우.

--- designpatten/Patten5/Client.java
+++ designpatten/Patten5/Client.java
@@ -3,8 +3,12 @@ package Patten5;
 public class Client {
 	public static void main(String[] args) {
 		ScoreRecord record =new ScoreRecord();
+		DataSheetView dataSheetView3= new DataSheetView(record, 3);
+		DataSheetView dataSheetView5= new DataSheetView(record, 5);
 		MinMaxView minMaxView = new MinMaxView(record);
 		
+		record.addDataSheetView(dataSheetView3);
+		record.addDataSheetView(dataSheetView5);
 		record.setMinMaxView(minMaxView);
 		
 		for (int i = 1; i <= 5; i++) {
--- designpatten/Patten5/ScoreRecord.java
+++ designpatten/Patten5/ScoreRecord.java
@@ -5,15 +5,25 @@ import java.util.List;
 
 public class ScoreRecord {
 	private List<Integer> scores = new ArrayList<Integer>();
-	private MinMaxView maxView;
+
+	private List<DataSheetView> dataSheetViews = new ArrayList<>();
+	private MinMaxView maxView ;
 	
-	public void setMinMaxView(MinMaxView maxView) { 
-		this.maxView = maxView;
+	public void addDataSheetView(DataSheetView dataSheetView) {
+		dataSheetViews.add(dataSheetView);
+	}
+	
+	public void setMinMaxView(MinMaxView minMaxView) {
+		this.maxView = minMaxView;
 	}
 	
 	public void addScore(int score) {
 		scores.add(score);
-		maxView.update();
+	 for(DataSheetView dataSheetView:dataSheetViews)
+		 dataSheetView.update();
+		
+		 maxView.update();
+	
 	}
 	public List<Integer> getScoreRecord(){
 		return scores;
