438433d6aa4e61d8c192556a3e33c0b9a16310d4
ijuhom <2014brand@naver.com>
2019-05-04 14:47:34 +0900

    Generic의 와일드카드 와 타입 정의의 이해 2

--- java/generic/java/Generics7.java
+++ java/generic/java/Generics7.java
@@ -14,14 +14,14 @@ public class Generics7{
 	}
 	//wild card 로 정의
 	static long frequency1(List<?> list, Object elem) {
-		return list.stream().filter(s -> s==elem).count();
+		return list.stream().filter(s -> s.equals(elem)).count();
 	}
 	public static void main(String[] args) {
-		List<Integer> list = Arrays.asList(1,2,3,4,5,3);
+		List<String> list = Arrays.asList("1","2","3","4","5","3");
 		System.out.println(isEmpty(list));
 		//와일드 카드로 사용하는것이 더 적합하다.
 		//generic으로 하게되면 내부 구현에서 T타입을 노출, api 의도가 바르지않다고 함 
-		System.out.println(frequency1(list,3));
-		System.out.println(frequency(list,3));
+		System.out.println(frequency1(list,"3"));
+		System.out.println(frequency(list,"3"));
 	}
 }
