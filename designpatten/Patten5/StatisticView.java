71a5d2492d38072db888d4a9e15676d050b7562a
ijuhom <2014brand@naver.com>
2019-05-04 14:40:43 +0900

    Generic의 와일드카드 와 타입 정의의 이해

---
+++ designpatten/Patten5/StatisticView.java
@@ -0,0 +1,23 @@
+package Patten5;
+
+import java.util.List;
+
+public class StatisticView implements Observer {
+	private ScoreRecord score;
+	
+	public StatisticView(ScoreRecord score) {
+		this.score = score;
+	}
+
+	@Override
+	public void update() {
+		List<Integer> list = score.getScoreRecord();
+		displayStatistic(list);
+	}
+
+	private void displayStatistic(List<Integer> list) {
+		int sum =0;
+		list.stream().mapToInt(i->i).average().ifPresent(avg -> System.out.println("Average found is " + avg));
+	}
+
+}
