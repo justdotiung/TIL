e459d11d09725742a806dc92b22dc509ca559b9c
ijuhom <2014brand@naver.com>
2019-05-08 23:50:12 +0900

    observer패턴의 이해 및 구조

--- designpatten/Patten5/Client.java
+++ designpatten/Patten5/Client.java
@@ -2,20 +2,26 @@ package Patten5;
 
 public class Client {
 	public static void main(String[] args) {
-		ScoreRecord record =new ScoreRecord();
-		DataSheetView dataSheetView3= new DataSheetView(record, 3);
-		DataSheetView dataSheetView5= new DataSheetView(record, 5);
-		MinMaxView minMaxView = new MinMaxView(record);
-		
+		ScoreRecord record = new ScoreRecord();
+		DataSheetView dataSheetView3 = new DataSheetView(record, 3);
 		record.attach(dataSheetView3);
-		record.attach(dataSheetView5);
+		MinMaxView minMaxView = new MinMaxView(record);
 		record.attach(minMaxView);
-		
+
 		for (int i = 1; i <= 5; i++) {
 			int sco = i * 10;
-			System.out.println("adding "+sco);			
+			System.out.println("adding " + sco);
 			record.addScore(sco);
 		}
-//		System.out.println(record.getScoreRecord().get(4));
+		record.detach(dataSheetView3);
+		StatisticView statisticView = new StatisticView(record);
+		record.attach(statisticView);
+
+		for (int i = 1; i <= 5; i++) {
+			int sco = i * 10;
+			System.out.println("adding " + sco);
+			record.addScore(sco);
+
+		}
 	}
 }
